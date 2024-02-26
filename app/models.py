82c561396ffd09d810b50f694c6c3c1afc82a63f
Camille <camille.yver01@gmail.com>
2024-02-27 00:21:41 +0100

    Fix foreign key capitalization and add many-to-many relationship between RawMaterial and Recipe

--- app/models.py
+++ app/models.py
@@ -4,12 +4,19 @@ class RawMaterial(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
     price = db.Column(db.Float, nullable=True)
-    supplier_id = db.Column(db.Integer, db.ForeignKey('supplier.id'), nullable=True)
-
+    supplier_id = db.Column(db.Integer, db.ForeignKey('Supplier.id'), nullable=True)
+    # This is a many-to-many relationship
+    recipes = db.relationship('Recipe', secondary='recipe_raw_material', lazy='subquery', backref=db.backref('raw_materials', lazy=True))
     def __repr__(self):
         return f'<RawMaterial {self.name}>'
     
-class supplier(db.Model):
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), unique=True, nullable=False)
+    # This is a many-to-many relationship
+    raw_materials = db.relationship('RawMaterial', secondary='recipe_raw_material', lazy='subquery', backref=db.backref('recipes', lazy=True))
+
+class Supplier(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), unique=True, nullable=False)
     # This is a one-to-many relationship
